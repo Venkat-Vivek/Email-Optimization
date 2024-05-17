67ffb246893abbae36d7b712e8add51b3bdd13db
Venkat-Vivek <simhadrivenkatvivek@gmail.com>
2024-05-17 23:33:52 +0530

    changed url

--- frontend/user-app/src/App.js
+++ frontend/user-app/src/App.js
@@ -1,116 +1,3 @@
-
-// import React, {  useState, useEffect} from 'react';
-// import axios from 'axios'
-// import './App.css';
-// import PieChart from './components/PieChart';
-
-// const App=()=> {
-
-//   // State to manage form input values
-//   const [formData, setFormData] = useState({
-//     subject1: '',
-//     content1: '',
-//     subject2: '',
-//     content2: ''
-//   });
-
-//   // Function to handle form input changes
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData(prevState => ({
-//       ...prevState,
-//       [name]: value
-//     }));
-//   };
-
-//   // Function to handle form submission
-//   const handleSubmit = async (e) => {
-//     // Logic to handle form submission (e.g., send data to backend)
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post('https://email-optimization.vercel.app/send-email', formData);
-//       console.log('Form submitted successfully:', response.data);
-//     } catch (error) {
-//       console.error('Error submitting form:', error);
-//     }
-//   };
-
-
-// const [experimentData, setExperimentData] = useState(null);
-
-// const fetchData = async () => {
-//   try {
-//     const response = await fetch('https://email-optimization.vercel.app/call');
-//     const data = await response.json();
-//     setExperimentData(data);
-//   } catch (error) {
-//     console.error('Error fetching experiment data:', error);
-//   }
-// };
-
-// useEffect(() =>{
-//   fetchData();
-// }, []);
-
-// const call1 = ()=>{
-//   fetch('https://email-optimization.vercel.app/call1')
-//   .then(res=>res.json())
-//   .then(json=>console.log(json))
-//   const data = {exp1:0, exp2:0}
-//   setExperimentData(data);
-// }
-
-//   return (
-//     <div>
-//       <h2>A/B Test Form</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label>Subject 1:</label>
-//           <input
-//             type="text"
-//             name="subject1"
-//             value={formData.subject1}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div>
-//           <label>Content 1:</label>
-//           <textarea
-//             name="content1"
-//             value={formData.content1}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div>
-//           <label>Subject 2:</label>
-//           <input
-//             type="text"
-//             name="subject2"
-//             value={formData.subject2}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div>
-//           <label>Content 2:</label>
-//           <textarea
-//             name="content2"
-//             value={formData.content2}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <button type="submit">Submit</button>
-//       </form>
-//       <button onClick={fetchData}>Get Analysis</button>
-      
-//     <div>
-//       {experimentData && <PieChart data={experimentData} />}
-//     </div>
-//     <button onClick={call1}>Reset</button>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
@@ -166,10 +53,11 @@ const App = () => {
 
   return (
     <div className="form-container">
-      <h2 className='heading'>A/B Test Form</h2>
+      <h1 className='heading'>A/B Test Form</h1>
       <form onSubmit={handleSubmit}>
+      <h2 className='heading'>Experiment 1</h2>
         <div className="form-group">
-          <label>Subject 1:</label>
+          <label>Subject:</label>
           <input
             type="text"
             name="subject1"
@@ -178,15 +66,16 @@ const App = () => {
           />
         </div>
         <div className="form-group">
-          <label>Content 1:</label>
+          <label>Content:</label>
           <textarea
             name="content1"
             value={formData.content1}
             onChange={handleChange}
           />
         </div>
+        <h2 className='heading'>Experiment 2</h2>
         <div className="form-group">
-          <label>Subject 2:</label>
+          <label>Subject:</label>
           <input
             type="text"
             name="subject2"
@@ -195,7 +84,7 @@ const App = () => {
           />
         </div>
         <div className="form-group">
-          <label>Content 2:</label>
+          <label>Content:</label>
           <textarea
             name="content2"
             value={formData.content2}
@@ -211,6 +100,13 @@ const App = () => {
         <button onClick={call1}>Reset</button>
       </div>
       <div>
+      {experimentData && experimentData.exp1 !== null && experimentData.exp2 !== null && experimentData.exp1 > experimentData.exp2 ? (
+  <p>Experiment 1 is more likely to yield favorable outcomes</p>
+) :experimentData && experimentData.exp1 !== null && experimentData.exp2 !== null && experimentData.exp2 !== 0 ? (
+  <p>Experiment 2 is more likely to yield favorable outcomes</p>
+) : null}
+
+
         {experimentData && <PieChart data={experimentData} />}
       </div>
     </div>

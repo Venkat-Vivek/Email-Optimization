51eb0ab0723fe3934ce490106c169973a70c6627
Venkat-Vivek <simhadrivenkatvivek@gmail.com>
2024-05-17 13:10:59 +0530

    changed url

--- frontend/user-app/src/App.js
+++ frontend/user-app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const App=()=> {
   
 const getUser=()=>{
-  fetch('https://email-optimization.vercel.app//get')
+  fetch('https://email-optimization.vercel.app/get')
   .then(res=>res.json())
 .then(json=>console.log(json))
 }

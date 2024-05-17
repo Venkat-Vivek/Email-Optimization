335bb05b76e4d4f7614bea935e7346f22561217b
Venkat-Vivek <simhadrivenkatvivek@gmail.com>
2024-05-17 17:16:15 +0530

    changed url

--- backend/index.js
+++ backend/index.js
@@ -137,8 +137,8 @@ const sendEmail = (recipientEmail, subject, content) => {
 app.get('/tracking-pixel', (req, res) =>{
   console.log("+1");
   // res.send({hi:"name"})
-  // const pixelPath = path.join(backend, 'pixel.png');
-  fs.readFile('pixel.png', (err, data) => {
+  const pixelPath = path.join(__dirname, 'pixel.png');
+  fs.readFile(pixelPath, (err, data) => {
     if (err) {
       console.error('Error reading pixel file:', err);
       res.sendStatus(500);
